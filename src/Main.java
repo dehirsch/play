dadb71cd1c642d1e5e42d6a10fca9c6e438484d9
David Hirsch <hirsch@igware.com>
2021-09-17 11:42:29 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,81 @@
+public class Main {
+
+
+    public static int fibonacci(int n) {
+        //calculate the nth fibonacci numbers
+        assert(n >= 0);
+        switch (n) {
+            case 0:
+                return 0;
+            case 1:
+                return 1;
+            default:
+                return (fibonacci(n - 1) + fibonacci(n - 2));
+
+        }
+
+
+    }
+
+    public static int dynamic_fibonacci(int n) {
+        //calculate the nth fibonacci numbers
+        assert(n >= 0);
+        int fsave[] = new int[n+2];
+        fsave[0] = 0;
+        fsave[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            fsave[i] = fsave[i-1] + fsave[i-2];
+        }
+        return fsave[n];
+
+
+    }
+
+    public static String[] combos(char[][] inputarray) {
+        return combos_int(inputarray, 0, new String[0]);
+    }
+
+    public static String[] combos_int(char[][] inputarray, int pos, String[] resultSoFar) {
+        char[] pos_array = inputarray[pos];
+
+        if (pos == inputarray.length-1) {
+            String[] resultArray = new String[pos_array.length];
+            int i = 0;
+            for (char ch : pos_array) {
+                resultArray[i] = Character.toString(ch);
+                i++;
+            }
+            return resultArray;
+        }
+        resultSoFar = combos_int(inputarray, pos+1, resultSoFar);
+
+        String[] resultArray = new String[resultSoFar.length * pos_array.length];
+        int j = 0;
+        for (char ch : pos_array) {
+            for (String res: resultSoFar) {
+                    resultArray[j] = Character.toString(ch) + res;
+                    j++;
+            }
+        }
+        return resultArray;
+
+        }
+    public static void main(String[] args) {
+        System.out.println("Hello World!999");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("fib " + Integer.toString(i)  + " : " + fibonacci(i));
+
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("d fib " + Integer.toString(i)  + " : " + dynamic_fibonacci(i));
+
+        }
+        char[][] x1 = {{'a','b', 'c'}, {'d', 'e', 'f'}, {'g', 'h', 'k'}};
+        String[] r1 = combos(x1);
+        System.out.println("R1 is" + java.util.Arrays.toString(r1));
+        System.out.println(("R1 has size: "  + Integer.toString(r1.length)));
+
+    }
+}
