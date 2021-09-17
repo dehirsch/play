a257acc9b024e4bc742e9f8ee8a545ec0b301c7d
David Hirsch <hirsch@igware.com>
2021-09-17 11:50:43 -0700

    Fixed warnings

--- src/Main.java
+++ src/Main.java
@@ -20,25 +20,25 @@ public class Main {
     public static int dynamic_fibonacci(int n) {
         //calculate the nth fibonacci numbers
         assert(n >= 0);
-        int fsave[] = new int[n+2];
-        fsave[0] = 0;
-        fsave[1] = 1;
+        int[] fSave = new int[n+2];
+        fSave[0] = 0;
+        fSave[1] = 1;
         for (int i = 2; i <= n; i++) {
-            fsave[i] = fsave[i-1] + fsave[i-2];
+            fSave[i] = fSave[i-1] + fSave[i-2];
         }
-        return fsave[n];
+        return fSave[n];
 
 
     }
 
-    public static String[] combos(char[][] inputarray) {
-        return combos_int(inputarray, 0, new String[0]);
+    public static String[] combos(char[][] inputArray) {
+        return combos_int(inputArray, 0, new String[0]);
     }
 
-    public static String[] combos_int(char[][] inputarray, int pos, String[] resultSoFar) {
-        char[] pos_array = inputarray[pos];
+    public static String[] combos_int(char[][] inputArray, int pos, String[] resultSoFar) {
+        char[] pos_array = inputArray[pos];
 
-        if (pos == inputarray.length-1) {
+        if (pos == inputArray.length-1) {
             String[] resultArray = new String[pos_array.length];
             int i = 0;
             for (char ch : pos_array) {
@@ -47,13 +47,13 @@ public class Main {
             }
             return resultArray;
         }
-        resultSoFar = combos_int(inputarray, pos+1, resultSoFar);
+        resultSoFar = combos_int(inputArray, pos+1, resultSoFar);
 
         String[] resultArray = new String[resultSoFar.length * pos_array.length];
         int j = 0;
         for (char ch : pos_array) {
             for (String res: resultSoFar) {
-                    resultArray[j] = Character.toString(ch) + res;
+                    resultArray[j] = ch + res;
                     j++;
             }
         }
@@ -64,18 +64,18 @@ public class Main {
         System.out.println("Hello World!999");
 
         for (int i = 0; i < 10; i++) {
-            System.out.println("fib " + Integer.toString(i)  + " : " + fibonacci(i));
+            System.out.println("fib " + i + " : " + fibonacci(i));
 
         }
 
         for (int i = 0; i < 10; i++) {
-            System.out.println("d fib " + Integer.toString(i)  + " : " + dynamic_fibonacci(i));
+            System.out.println("d fib " + i + " : " + dynamic_fibonacci(i));
 
         }
         char[][] x1 = {{'a','b', 'c'}, {'d', 'e', 'f'}, {'g', 'h', 'k'}};
         String[] r1 = combos(x1);
         System.out.println("R1 is" + java.util.Arrays.toString(r1));
-        System.out.println(("R1 has size: "  + Integer.toString(r1.length)));
+        System.out.println(("R1 has size: "  + r1.length));
 
     }
 }
